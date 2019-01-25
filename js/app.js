7eff32098c0f08a7134a92a1c228fcf4bc874392
Rene.Mendes <gwingster@gmail.com>
2019-01-24 21:08:10 -0800

    had to change it back to a working code

--- js/app.js
+++ js/app.js
@@ -1,22 +1,11 @@
-//The ready event occurs when the DOM (document object model) has been loaded
 $(document).ready(function () {
+  
+  
 
-
-
-
-
-  $(function () {
-    $("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
-  });
-
-
-  // gif array that will have the inital button set.
   var gifsArr = ["gundam", "pokemon", "outlaw star", "trigun", "full metal alchemist", "Dragon Ball"];
 
-
-
   function gifBtn() {
-    $("#buttonsHere").empty(); //empties the page to keep duplicates from being made
+    $("#buttonsHere").empty();
     for (var i = 0; i < gifsArr.length; i++) {
       var gifButton = $("<button>");
       gifButton.addClass("gifRequest btn btn-dark");
@@ -25,53 +14,32 @@ $(document).ready(function () {
       gifButton.text(gifsArr[i]);
       $("#buttonsHere").append(gifButton);
     }
+
   }
 
 
 
-  //Allows the user to input a search, push into the gifsArr which is then pulls into the gifBtn Function
+
   function userDynamicBtn() {
     $("#addGif").on("click", function () {
       var gifRequest = $("#gifInput").val().trim();
-      if (gifsArr.indexOf(gifRequest) === -1) {
-        gifsArr.push(gifRequest);
-        gifBtn();
-      } else {
-        $(function () {
-          $("#dialog-message").dialog({
-            modal: true,
-            buttons: {
-              Ok: function () {
-                $(this).dialog("close");
-              }
-            }
-          });
-        });;
+      //if (find_duplicate_in_array(gifsArr) == gifRequest){
+      //  alert("You have already chosen that, choose another.");
+      //  input.value = false;} 
+      if (gifRequest == "" /*|| gifRequest == find_duplicate_in_array(gifsArr) need to figure out how to tell if there is a duplicate request*/) {
+        input.value = false;
       }
-      $("#form").val("")
-    });
-  }
-
-
-
-  //Allows the user to input a search, push into the gifsArr which is then pulls into the gifBtn Function
-  function userDynamicBtn() {
-    $("#addGif").on("click", function () {
-      var gifRequest = $("#gifInput").val().trim();
-      if (gifsArr.indexOf(gifRequest) === -1) {
-        gifsArr.push(gifRequest);
-        renderButtons();
-      } else {
-        alert("Please type a different show");
-      }
+      $('form').val("");
+      gifsArr.push(gifRequest);
       gifBtn();
+      return false;
     });
   }
 
   function displayGifs() {
     var gifRequest = $(this).attr("data-name");
     var queryURL =
-      "http://api.giphy.com/v1/gifs/search?q=" + gifRequest + "&api_key=dc6zaTOxFJmzC&limit=4";
+      "http://api.giphy.com/v1/gifs/search?q=" + gifRequest + "&api_key=dc6zaTOxFJmzC&limit=5";
     $.ajax({
       url: queryURL,
       method: 'GET'
@@ -80,13 +48,13 @@ $(document).ready(function () {
       $("#gifsHere").empty();
       var gifRequest = response.data;
       if (gifRequest == "") {
-        alert("Thee isn't a gif for this selected button");
+        alert("There isn't a gif for this selected button");
       }
       for (var i = 0; i < gifRequest.length; i++) {
 
         var gifDiv = $("<div>");
         gifDiv.addClass("sm-4");
-        var gifImage = $("<img>", "<br>");
+        var gifImage = $("<img>");
         gifImage.attr("src", gifRequest[i].images.fixed_height_still.url);
         gifImage.attr("data-still", gifRequest[i].images.fixed_height_still.url);
         gifImage.attr("data-animate", gifRequest[i].images.fixed_height.url);
@@ -94,7 +62,6 @@ $(document).ready(function () {
         gifImage.attr("class", "image");
         $(gifDiv).append(gifImage);
         $("#gifsHere").append(gifDiv);
-
       }
     });
   }
@@ -115,5 +82,5 @@ $(document).ready(function () {
 
   $(function () {
     $("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
-  });
+    });
 });
